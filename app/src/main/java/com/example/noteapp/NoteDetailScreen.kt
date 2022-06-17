259b7788b2dd9043385833597a3fa116c5b7b1d7
Cedric Maubach <cedricmaubach@web.de>
2022-06-17 09:24:58 +0200

    remove unused imports

--- app/src/main/java/com/example/noteapp/NoteDetailScreen.kt
+++ app/src/main/java/com/example/noteapp/NoteDetailScreen.kt
@@ -1,7 +1,5 @@
 package com.example.noteapp
 
-import android.util.Size
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -12,8 +10,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.*
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
